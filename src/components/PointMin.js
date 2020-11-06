6337d4a2923d0426ee45f986f27393cb2a8a8e17
Scket4 <alexeygolubev921@gmail.com>
2020-11-06 14:14:49 +0300

    made response slider

--- src/components/PointMin.js
+++ src/components/PointMin.js
@@ -5,7 +5,7 @@ export class PointMin extends SliderComponent {
   constructor(emitter, $root, values) {
     super(emitter, $root, {
       name: 'Point',
-      listeners: ['mousedown']
+      listeners: ['mousedown', 'touchstart']
     }, values)
   }
 
@@ -20,10 +20,31 @@ export class PointMin extends SliderComponent {
     if (prop === 'sliderStart' || prop === 'sliderEnd')  this.prop.sliderSize = this.prop.sliderEnd - this.prop.sliderStart
   }
 
-  makeChange(prop, val) {
+  makeChange() {
     this.$root.left(this.prop.positionMin * this.prop.slider.width) 
     this.prop.pointMinX = this.$root.$el.getBoundingClientRect().x + 12.5
 
+  }
+
+  onTouchstart(e) {
+    const shift = e.changedTouches[0].clientX - this.prop.pointMinX
+
+    document.ontouchmove = e => {
+      let left = e.changedTouches[0].clientX - this.prop.slider.x - shift
+      console.log(this.prop.slider);
+      left = left > this.prop.slider.width ? this.prop.slider.width : left < 0 ? 0 : left
+
+      const onePercent = this.prop.slider.width / 100
+      if (this.prop.isRange == 1) left = left >= (this.prop.positionMax * this.prop.slider.width) - onePercent // Проверка если больше чем pointMax
+      ? (this.prop.positionMax * this.prop.slider.width) - onePercent : left
+
+      this.emitter.trigger('viewToPresenter', {positionMin: left / this.prop.slider.width})
+
+      document.ontouchend = () => {
+        document.onMousedown = null
+        document.onmousemove = null
+        }
+      }
   }
 
   onMousedown(e) {
