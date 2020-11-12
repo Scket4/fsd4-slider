da476fc02b6b88c71839ff1da901480b4f5bbed8
Scket4 <alexeygolubev921@gmail.com>
2020-11-12 15:16:26 +0300

    init typescript

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,20 @@ module.exports = {
         }
       },
       {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript'
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
       test: /\.ttf$/,
       use: ['file-loader']
       }
